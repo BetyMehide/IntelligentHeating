03795685dcaa23e99f73326267d2e831b080498b
Bety Mehide <18532153+BetyMehide@users.noreply.github.com>
2018-02-21 22:24:04 -0800

    Two extra PIR sensors and 2 physical buttons added
    The physical buttons can be pressed separately or together.

--- sensorClient.ino
+++ sensorClient.ino
@@ -3,38 +3,43 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
-#include <TimeLib.h>
+//#include <TimeLib.h>
 
 //DEFINE VARIABLES
-int pirValue; // Place to store read PIR Value
+int pirValue1; // Place to store the PIR (movement) sensor Value
+int pirValue2;
+int pirValue3;
+int moveInLastPeriod; // Variable to remember whether there's been any movement in the last 10 minutes
+int prevButtonUpState = LOW;
+int prevButtonDownState = LOW;
 //variables for controlling information sensing time
 unsigned long curMillis; 
-long tempDelay = 10000; //delay for displaying temp and humidity data in milliseconds
+long tempDelay = 100000; //delay for sending temp and humidity data in milliseconds
 long prevTempMillis = 0;
-long motionDelay = 2000; //delay for displaying motion sensor data
-long prevMotionMillis = 0;
-int prevTouchValueUp = LOW;
-int prevTouchValueDown = LOW;
+int pressDelay;
+long buttonPressDelay = 1200; //delay for reading the button presses
+long curPressStart = 5000000;
+int currentlyPressed = 0;
 //variables for wifi connection
-const char* ssid =  "****";     // replace with your wifi ssid and wpa2 key
+const char* ssid =  "*****";     // replace with your wifi ssid and wpa2 key
 //const char* password =  "****";
 int status = WL_IDLE_STATUS;
-IPAddress server(****); //server address
-const int httpPort = 5000;
+IPAddress server(*****); //server address
+const int httpPort = 5001;
 HTTPClient http; 
 //json variables
 StaticJsonBuffer<500> JSONBuffer; //setup memory pool for the JSON object tree
 JsonObject& data = JSONBuffer.createObject();
 char JSONmessageBuffer[500];
-//time variables
-time_t t = now();
 
 //DEFINE PINS
-#define DHTPIN 15 // pin for the temp and humidity sensor
+#define DHTPIN 16 // pin for the temp and humidity sensor
 #define DHTTYPE DHT22 //the humidity sensor type
-int pirPin = 16; // Input for HC-S501 (motion sensor)
-#define ctsPinUp 4 //touch sensor for up
-#define ctsPinDown 0 //touch sensor for down
+int pirPin1 = 12; // Input for HC-S501 (motion sensor)
+int pirPin2 = 13;
+int pirPin3 = 15;
+int buttonUpPin = 4; //touch sensor for up
+int buttonDownPin = 2; //touch sensor for down
 
 //INIT
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor
@@ -43,6 +48,7 @@ WiFiClient client; //initialize the client library
 void setup(){
   Serial.begin(9600);
   delay(10);
+  pinMode(LED_BUILTIN, OUTPUT);
   
   //WIFI PORT SETUP
   Serial.println("Attempting to connect to WPA network...");
@@ -69,12 +75,14 @@ void setup(){
   //TEMPERATURE AND HUMIDITY SENSOR SETUP
   dht.begin();
 
-  //setup for the touch sensor
-  pinMode(ctsPinUp, INPUT);
-  pinMode(ctsPinDown, INPUT);
+  //BUTTONS SETUP
+  pinMode(buttonUpPin, INPUT);
+  pinMode(buttonDownPin, INPUT);
 
-  //setup for motion sensor
-  pinMode(pirPin, INPUT);
+  //MOTION SENSORS SETUP
+  pinMode(pirPin1, INPUT);
+  pinMode(pirPin2, INPUT);
+  pinMode(pirPin3, INPUT);
 }
 
 
@@ -82,15 +90,21 @@ void setup(){
 void recData(){
   data["temperature"] = dht.readTemperature(true);
   data["humidity"] = dht.readHumidity();
-  data["movement"] = pirValue;
-  String timev = String(month(t)) + " " + String(day()) + " " + String(weekday()) + " " + String(hour()) + " " + String(minute()); 
-  Serial.println(timev);
+  data["movement"] = moveInLastPeriod;
+    Serial.println("Data sending in process....");
+    Serial.print("Temperature in F = ");
+    Serial.println(dht.readTemperature(true));
+    Serial.print("Temperature in C =");
+    Serial.println(dht.readTemperature());
+    Serial.print("Humidity = ");
+    Serial.println(dht.readHumidity());
+    Serial.print("Move in the last time period = ");
+    Serial.println(moveInLastPeriod);
+  
+  //String timev = String(month(t)) + " " + String(day(t)) + " " + String(weekday(t)) + " " + String(hour(t)) + " " + String(minute(t)); 
+  //Serial.println(timev);
   //JsonArray& timestamps = data.createNestedArray("timestamps");
-  data["timestamps"] = timev;
-  //timestamps.add(day(t));
-  //timestamps.add(weekday(t));
-  //timestamps.add(hour(t));
-  //timestamps.add(minute(t));
+  //data["timestamps"] = timev;
 }
 
 //POST DATA TO SERVER
@@ -102,11 +116,20 @@ void clientHandling(){
     Serial.println(JSONmessageBuffer);
 
     //handle the http request
-    http.begin("http://40.71.19.247:5000/post/logdata");
+    http.begin("http://40.71.19.247:5001/post/logdata");
     http.addHeader("Content-Type", "application/json");
+    Serial.println("Data sending in process....");
     int httpCode = http.POST(JSONmessageBuffer);
     String payload = http.getString();
+    Serial.print("Server response = ");
     Serial.println(httpCode);
+    if (httpCode != 200){
+      Serial.println("failed to send data");
+      digitalWrite(LED_BUILTIN, LOW);      
+    }
+    else{
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
     Serial.println(payload);
     http.end();
   }
@@ -117,60 +140,64 @@ void clientHandling(){
 
 void loop()
 {
-  curMillis = millis();
+  curMillis = millis();  
 
-  //TEMPERATURE AND HUMIDITY SENSOR HANDLING
+  //DATA SENDING EVERY 10 MINUTES
   if(curMillis - prevTempMillis >= tempDelay){
     prevTempMillis = curMillis;
-    Serial.print("Temperature in F = ");
-    Serial.println(dht.readTemperature(true));
-    Serial.print("Temperature in C =");
-    Serial.println(dht.readTemperature());
-    Serial.print("Humidity = ");
-    Serial.println(dht.readHumidity());
+    recData();
+    clientHandling();
+    moveInLastPeriod = 0;
   }
 
   //MOTION SENSOR HANDLING
-  pirValue = digitalRead(pirPin);
-  if (pirValue){
-    if(curMillis - prevMotionMillis >= motionDelay){
-      prevMotionMillis = curMillis;
-      Serial.print("Movement = ");
-      Serial.println(pirValue);
-      //send relevant data to server
-      recData();
-      clientHandling();
-    } 
-  }
-  if (!pirValue){
-    prevMotionMillis = 0;  
+  pirValue1 = digitalRead(pirPin1);
+  pirValue2 = digitalRead(pirPin2);
+  pirValue3 = digitalRead(pirPin3);
+  if (pirValue1 || pirValue2 || pirValue3){
+    moveInLastPeriod = 1;
+    Serial.print(pirValue1);
+    Serial.print(pirValue2);
+    Serial.println(pirValue3);
   }
   
-  //TOUCH SENSOR HANDLING
-  int touchValueUp = digitalRead(ctsPinUp);
-  if(touchValueUp == HIGH){
-    if(prevTouchValueUp != HIGH){
-      Serial.println("Up");
-      //data["user"] = 1;
-      recData();
-      clientHandling();  
-    }
-    prevTouchValueUp = touchValueUp;
+  //BUTTONS HANDLING
+  int buttonUpState = digitalRead(buttonUpPin);
+  int buttonDownState = digitalRead(buttonDownPin);
+  //control for button pressing time
+  if ((buttonUpState == HIGH || buttonDownState == HIGH) && currentlyPressed == 0){
+    curPressStart = curMillis;
+    currentlyPressed = 1;
   }
-  else{
-    prevTouchValueUp = LOW;
+  if (curMillis - curPressStart >= buttonPressDelay && currentlyPressed == 1){
+    pressDelay = 1;
   }
-  int touchValueDown = digitalRead(ctsPinDown);
-  if(touchValueDown == HIGH){
-    if(prevTouchValueDown != HIGH){
-      Serial.println("Down");
-      //data["user"] = -1;
-      recData();
-      clientHandling();  
-    }
-    prevTouchValueDown = touchValueDown;
+  //if both buttons pressed
+  if(buttonUpState == HIGH && buttonDownState == HIGH && (prevButtonUpState == LOW || prevButtonDownState == LOW)){
+    Serial.println("Comfortable");
+    prevButtonUpState = HIGH;
+    prevButtonDownState = HIGH;
+    pressDelay = 0;
   }
-  else{
-    prevTouchValueDown = LOW;
+  //if too cold button pressed
+  else if(buttonUpState == HIGH && prevButtonUpState == LOW && pressDelay == 1){
+    Serial.println("Up botton pressed");
+    prevButtonUpState = buttonUpState;
+    pressDelay = 0;
+  }
+  //if too warm button pressed
+  else if(buttonDownState == HIGH && prevButtonDownState == LOW && pressDelay == 1){
+    Serial.println("Down button pressed");
+    prevButtonDownState = buttonDownState;
+    pressDelay = 0;
+  }
+
+  if(prevButtonDownState == HIGH && buttonDownState == LOW){
+    prevButtonDownState = LOW;
+    currentlyPressed = 0;
+  }
+  if(prevButtonUpState == HIGH && buttonUpState == LOW){
+    prevButtonUpState = LOW;
+    currentlyPressed = 0;
   }
 }    
